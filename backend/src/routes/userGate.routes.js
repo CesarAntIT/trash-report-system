39ea691639dbc8e45fb04ef8ab1ca9bdb4965ebb
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2026-04-05 18:59:13 -0400

    feat: Nuevo Ruta Agregada Para Iniciar Session

--- backend/src/routes/userGate.routes.js
+++ backend/src/routes/userGate.routes.js
@@ -6,12 +6,7 @@ const RequestUserSolicitudeApi = require('../controllers/users.controller.js')
 
 const router = Router()
 //Metodo Post Crear Usuario
-<<<<<<< HEAD
-router.post('/', RequestUserSolicitudeApi.CrearUsuario)
-
-=======
 router.post('/users', RequestUserSolicitudeApi.CrearUsuario)
 //Metodo Post Iniciar Session
 router.post('/login', RequestUserSolicitudeApi.IniciarSesion)
->>>>>>> 825b0d5 (feat: Nuevo Ruta Agregada Para Iniciar Session)
 module.exports = router
