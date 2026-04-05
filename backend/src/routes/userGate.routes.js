2bd4c2a7acc6603173ad4979a90392b17fc3a6dd
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2026-04-05 19:51:37 -0400

    feat: Cambios

--- backend/src/routes/userGate.routes.js
+++ backend/src/routes/userGate.routes.js
@@ -7,10 +7,6 @@ const RequestUserSolicitudeApi = require('../controllers/users.controller.js')
 const router = Router()
 //Metodo Post Crear Usuario
 router.post('/users', RequestUserSolicitudeApi.CrearUsuario)
-<<<<<<< HEAD
 //Metodo Post Iniciar Session
 router.post('/login', RequestUserSolicitudeApi.IniciarSesion)
-=======
-
->>>>>>> 57a75317353f8cdc13726a738675ba99222070b8
 module.exports = router
